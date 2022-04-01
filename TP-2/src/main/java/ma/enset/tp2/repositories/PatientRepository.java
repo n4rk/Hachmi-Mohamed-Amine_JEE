bffc2516cd972ea556f40a4ee5b2536429041282
n4rk <m.hachmi_etu@enset-media.ac.ma>
2022-04-01 17:25:02 +0100

    Committed TP 2

---
+++ TP-2/src/main/java/ma/enset/tp2/repositories/PatientRepository.java
@@ -0,0 +1,20 @@
+package ma.enset.tp2.repositories;
+
+import ma.enset.tp2.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByMalade(boolean m);
+    Page<Patient> findByMalade(boolean m, Pageable pageable);
+    List<Patient> findByMaladeAndScoreLessThan(boolean m, int s);
+    List<Patient> findByDateNaissanceBetween(Date d1, Date d2);
+    @Query("select p from Patient p where p.dateNaissance between :x and :y or p.nom like :z")
+    List<Patient> chercherPatients(@Param("x") Date d1, @Param("x") Date d2, @Param("x") String mc);
+}
